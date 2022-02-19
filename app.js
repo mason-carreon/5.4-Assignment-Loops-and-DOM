16e0686173462bf6190a2659825a4450cb93d488
mason-carreon <70252484+mason-carreon@users.noreply.github.com>
2022-02-19 15:01:29 -0600

    5.4

---
+++ app.js
@@ -0,0 +1,17 @@
+const names = ["Floyd", "Zay", "Mason"];
+
+for (name of names) {
+    console.log (name);
+    if (name === "Mason") {
+        console.log ("Mason is in the list!");
+        break;
+    }
+}
+
+let loading = 0;
+
+while (loading < 100) {
+    console.log("Website still loading");
+
+    loading++;
+}
