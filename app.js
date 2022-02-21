b9a7d52ddfdd16b3ef491b57466132b75117a40d
mason-carreon <70252484+mason-carreon@users.noreply.github.com>
2022-02-20 18:40:52 -0600

    5.4

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+document.getElementById("p2").style.color = "blue";
+document.getElementById("p2").style.fontFamily = "Arial";
+document.getElementById("p2").style.fontSize = "larger";
+
 const names = ["Floyd", "Zay", "Mason"];
 
 for (name of names) {
